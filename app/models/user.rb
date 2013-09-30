3e2bb608eb51ed6de220ecb32a8fbe853c7422ed
alexandermitchell <alex.mitchell@mac.com>
2013-09-30 14:20:33 -0400

    Generating Project model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+  has_many :projects
+  attr_accessible :first_name, :last_name, :email, :password
 end
