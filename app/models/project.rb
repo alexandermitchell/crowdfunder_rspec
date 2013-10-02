0cb7075df78b9104b300e00d659c907d139b22b9
alexandermitchell <alex.mitchell@mac.com>
2013-10-02 15:51:07 -0400

    Generating factory for Pledge

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :pledges
-  
-  attr_accessible :description, :goal, :teaser, :title
 
+  attr_accessible :description, :goal, :teaser, :title
 
   validates :title, presence: true, length: {maximum: 100}
   validates :teaser, presence: true, length: {maximum: 255}
