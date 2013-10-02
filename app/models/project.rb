fa4d31ee3c1526a1eb613844bdd266842476c226
alexandermitchell <alex.mitchell@mac.com>
2013-10-02 15:49:11 -0400

    Generating Pledge model and adding associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  has_many :pledges
+  
   attr_accessible :description, :goal, :teaser, :title
+
 
   validates :title, presence: true, length: {maximum: 100}
   validates :teaser, presence: true, length: {maximum: 255}
